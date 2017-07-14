dfec2f79f2ea88b0971a621eab1e7021ec005d73
KyleSoon <562881821@qq.com>
2017-07-14 03:11:54 +0000

    dincichangshiyebunengfangqi

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
 // Write your cade below:
-module.exports = function main() {
-  
-};
+module.exports =
+function main(a, b){
+    var result = a % b;
+    return result;
+}
+//12345
+//comment by yang
